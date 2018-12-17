neos/contentgraph-doctrinedbaladapter

commit 7e7ff3594fcf4ef68bb747390f08c0afefad2f0f

Add "strict_types" declaration to all PHP files
According to https://discuss.neos.io/t/usage-of-php-strict-typing-by-default/3861

Classes/Domain/Repository/ContentSubgraph.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Repository;
 

Classes/Domain/Projection/GraphProjector.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Projection;
 

Classes/Domain/Projection/HierarchyRelation.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Projection;
 

Classes/Domain/Projection/Node.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Projection;
 

Classes/Domain/Projection/NodeAggregate.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Projection;
 

Classes/Domain/Projection/NodeRelationAnchorPoint.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Projection;
 

Classes/Domain/Repository/ContentGraph.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Repository;
 

Classes/Domain/Repository/NodeFactory.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Repository;
 

commit 01a310b64a4122c3eadeffebdcdd21154ba51950

Fix invalid types that break tests
Due to the `strict_types` declarations introduced with 7e7ff3594fcf4ef68bb747390f08c0afefad2f0f

Classes/Domain/Repository/ContentSubgraph.php
@@ -329,7 +329,7 @@ SELECT n.*, h.name, h.contentstreamidentifier, h.dimensionspacepoint FROM neos_c
         }
 
         $res = $query->execute($this->getDatabaseConnection())->fetchColumn(0);
-        return $res;
+        return (int)$res;
     }
 
     /**